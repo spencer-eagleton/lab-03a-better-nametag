c737ad05b8faf963eb073e73d16fe784dbd0b320
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-09-29 12:02:08 -0700

    added JS and HTML name input function but not working yet

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-// import functions and grab DOM elements
-
+// grab DOM elements
+const nameInput = document.getElementById('name');
+const button = document.getElementById('generate');
+const firstName = document.getElementById('first-name');
 // initialize global state
 
 // set event listeners 
+
+
+
+// when button clicked
+
+button.addEventListener('click', ()=>{
+  let newName = nameInput.value;
+  firstName.textContent = newName;
+});
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+
